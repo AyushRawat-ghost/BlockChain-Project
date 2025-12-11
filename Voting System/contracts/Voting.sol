bc89c5979f8121496abb4f6e67a3282247e0f977
Ayush Rawat <ayush6342ily@gmail.com>
2025-12-11 23:01:27 +0530

    Paused

--- Voting System/contracts/Voting.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import "./core/VotingCore.sol";
-
-contract Voting is VotingCore {
-    constructor(address admin_) VotingCore(admin_) {}
-}
